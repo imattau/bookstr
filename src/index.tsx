cde084a6cf24ac0e5a605f97aa25615649dc1db6
imattau <imatt.au@protonmail.com>
2025-07-26 16:16:46 +1000

    Add status to library items

--- src/index.tsx
+++ src/index.tsx
@@ -3,31 +3,19 @@ import { AppShell } from './AppShell';
 import { Header } from './components/Header';
 import { BottomNav } from './components/BottomNav';
 import { ThemeProvider } from './ThemeProvider';
-import { ThemeSwitcher } from './components/ThemeSwitcher';
-import { ProfileSettings } from './components/ProfileSettings';
 
 export const App: React.FC = () => {
   const [active, setActive] = React.useState<
     'discover' | 'library' | 'write' | 'activity' | 'profile'
   >('discover');
+
   return (
     <ThemeProvider>
-      <NostrProvider>
-        <AppShell>
-          <Header onSearch={() => {}}>
-            <ThemeSwitcher />
-          </Header>
-            {active === 'write' && <BookForm />}
-            {active === 'profile' && (
-              <div className="space-y-4">
-                <Login />
-                <ProfileSettings />
-              </div>
-            )}
-          </main>
-          <BottomNav active={active} onChange={setActive} />
-        </AppShell>
-      </NostrProvider>
+      <AppShell>
+        <Header onSearch={() => {}} />
+        <main />
+        <BottomNav active={active} onChange={setActive} />
+      </AppShell>
     </ThemeProvider>
   );
 };
