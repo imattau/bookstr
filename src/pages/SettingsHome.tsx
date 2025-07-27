6c2f2df797f317aeb7f9aac925d5d49ad1756e64
imattau <imatt.au@protonmail.com>
2025-07-27 19:56:33 +1000

    Remove unused settings pages

---
+++ src/pages/SettingsHome.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const SettingsHome: React.FC = () => (
+  <div className="space-y-2">
+    <ul className="list-disc pl-4 space-y-1">
+      <li>
+        <Link to="/settings/profile" className="text-primary-600 underline">
+          Profile
+        </Link>
+      </li>
+      <li>
+        <Link to="/settings/ui" className="text-primary-600 underline">
+          Appearance
+        </Link>
+      </li>
+      <li>
+        <Link to="/settings/offline" className="text-primary-600 underline">
+          Offline
+        </Link>
+      </li>
+    </ul>
+  </div>
+);
+
+export default SettingsHome;
